3a66d44b6e51e97fd2ef671fad765fbcaa706e26
Elizabeth Bartholomew <lizzy.bartholomew@gmail.com>
2023-11-18 14:22:04 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import ViewsCard from "./Components/ViewsCard";
 import './index.css';
 
 function App() {
+  const big_num = ["1987", "1044", "11k", "8239"];
   return (
     <>
     <div className="body">
@@ -15,10 +16,17 @@ function App() {
       </nav>
       
       <div className="section-1">
+        {/* <FollowersCard/>
         <FollowersCard/>
         <FollowersCard/>
-        <FollowersCard/>
-        <FollowersCard/>
+        <FollowersCard/> */}
+        {big_num.map((element, index) => {
+        return (
+          <div key={index} className="followers-card">
+            <h2>{element}</h2>
+          </div>
+        );
+      })}
       </div>
       <h2>Overview - Today</h2>
       <div className="section-2">
--- src/Components/FollowersCard.js
+++ src/Components/FollowersCard.js
@@ -1,6 +1,8 @@
 
 
 export default function FollowersCard() {
+    const big_num = ["1987", "1044", "11k", "8239"];
+    const big_percent = [12, 99, 1099, 144]
   return (
     <div className="followers-card">
         <div className="top">
@@ -8,7 +10,9 @@ export default function FollowersCard() {
             <div className="user-name">@nathanf</div>
         </div>
         <div className="middle">
-            <div className="followers-num">1987</div>
+            <div className="followers-num">
+            
+            </div>
             <div className="followers">FOLLOWERS</div>
         </div>
         <div className="bottom">
@@ -18,3 +22,5 @@ export default function FollowersCard() {
     </div>
   )
 }
+
+
